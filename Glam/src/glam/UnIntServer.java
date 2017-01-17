d1205a09b4b19280c44c46ffbc9a21a9c4e3b675
zhouzhiqiang <zhouzhiqiang@Lab06_05.itseinaudi.local>
2017-01-17 10:00:01 +0100

    commit

---
+++ Glam/src/glam/UnIntServer.java
@@ -0,0 +1,23 @@
+package glam;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class UnIntServer {
+	public static void main(String[] args) {
+		// Avvio il server
+		ServerSocket ss;
+		try {
+			ss = new ServerSocket(9080);
+			while (true) {				
+				Socket clientSocket = ss.accept(); // Accetta la connessione
+				System.out.println("Server: Ho ricevuto il numero " + clientSocket.getInputStream().read());
+			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+	}
+}
