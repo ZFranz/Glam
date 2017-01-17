f4815627326d887503c6f33b6db2550f2aa37120
zhouzhiqiang <zhouzhiqiang@Lab06_05.itseinaudi.local>
2017-01-17 10:32:27 +0100

    database

--- Glam/src/glam/Database.java
+++ Glam/src/glam/Database.java
@@ -3,5 +3,34 @@ package glam;
 import java.sql.*;
 
 public class Database {
+	public void Iscritto(String nome, Date data) {
+		Connection cn;
+		Statement st;
+		String sql;
 
+		// ________________________________connessione
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("ClassNotFoundException: ");
+			System.err.println(e.getMessage());
+		} // fine try-catch
+
+		try {
+			// Creo la connessione al database
+			cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/carsharing?user=root&password=");
+
+			// ________________________________query
+			sql = "INSERT INTO noleggi (auto, socio, inizio, fine, auto_in_uso, auto_restituita) VALUES ('" + targa
+					+ "', '" + cf + "', '" + dataInizio + "', '" + dataFine + "', 0, 0);";
+			System.out.println(sql);
+
+			st = cn.createStatement(); // creo sempre uno statement sulla
+										// connessione
+			st.execute(sql); // faccio la query su uno statement
+			cn.close(); // chiusura connessione
+		} catch (SQLException e) {
+			System.out.println("errore:" + e.getMessage());
+		} // fine try-catch
+	}
 }
--- Glam/src/glam/Server.java
+++ Glam/src/glam/Server.java
@@ -1,95 +1,32 @@
 package glam;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Text;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-
 public class Server {
-
-	protected Shell shlServer;
-	private Text text;
-
-	/**
-	 * Launch the application.
-	 * 
-	 * @param args
-	 */
 	public static void main(String[] args) {
+		String temp = "";
+
+		// Avvio il server
+		ServerSocket ss;
 		try {
-			Server window = new Server();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * Open the window.
-	 */
-	public void open() {
-		Display display = Display.getDefault();
-		createContents();
-		shlServer.open();
-		shlServer.layout();
-		while (!shlServer.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
+			ss = new ServerSocket(9999);
+			System.out.println("Server up and running.");
+			while (true) {
+				Socket s = ss.accept(); // Accetta la connessione
+				InputStreamReader isr = new InputStreamReader(s.getInputStream());
+				BufferedReader in = new BufferedReader(isr);
+				temp = in.readLine();
+				System.out.println("Il server riceve: " + temp);
+				s.close();
+				// riparta
 			}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-	}
-
-	/**
-	 * Create contents of the window.
-	 */
-	protected void createContents() {
-		shlServer = new Shell();
-		shlServer.setSize(450, 300);
-		shlServer.setText("Server");
-
-		Button btnAvviaServer = new Button(shlServer, SWT.NONE);
-		text = new Text(shlServer, SWT.BORDER);
-
-		btnAvviaServer.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e){
-				// Avvio il server
-				ServerSocket ss;
-				try {
-					ss = new ServerSocket(9999);
-					System.out.println("Server up and running.");
-					while (true) {
-						Socket s = ss.accept(); // Accetta la connessione
-						InputStreamReader isr = new InputStreamReader(s.getInputStream());
-						BufferedReader in = new BufferedReader(isr);
-						System.out.println("Il server riceve: " + in.readLine());
-						// invio il numero aumentato di uno
-						PrintWriter out = new PrintWriter(s.getOutputStream(), true);
-						out.println("ciao dal serverone!");
-						s.close();
-						// riparta
-					}
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
-		btnAvviaServer.setBounds(10, 10, 75, 25);
-		btnAvviaServer.setText("Avvia Server");
-
-		text.setBounds(10, 41, 76, 21);
-
 	}
 }
